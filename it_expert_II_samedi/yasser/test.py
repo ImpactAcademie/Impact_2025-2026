8838f0779013d41c8c1dd1d8431602b3ba5259e4
student <student@gmail.com>
2025-09-06 17:12:57 +0100

    yasser lesson 1

---
+++ it_expert_II_samedi/yasser/test.py
@@ -0,0 +1,21 @@
+# x = input("Quel est votre prénom ? ")
+# print('Bonjour,', x)
+
+vie = 11
+vie -= 1
+print(vie)
+
+if vie < 10:
+    print("Tu as moins de 10 vies ! (",vie,")")
+elif vie == 10:
+    print("Tu as 10 vies !")
+else:
+    print("Vous avez plus de 10 vies ! (",vie,")")
+x = "*"
+for i in range(5):
+      print(i)
+
+while vie > 0:
+    print("Je t'attaque")
+    vie -= 2
+    
