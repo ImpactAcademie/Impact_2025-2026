7730086f7f4dd4f3e884968a815940b605672561
student <student@gmail.com>
2025-09-25 19:10:26 +0200

    lessons 4

---
+++ it_expert_II_jeudi/ayoub/morpion.py
@@ -0,0 +1,28 @@
+def draw_grid(values):            
+     for j in range(3):
+        for i in range(3):
+            if i != 1:
+             print("       |       |       ")
+            else:
+               print(f"   {values[j][0]}   |   {values[j][1]}   |   {values[j][2]}   ")
+        if j < 2:
+            print("-------+-------+-------")
+
+values = [[" "," ", " "],
+          [" ", " ", " "],
+          [" ", " ", " "]]
+player_symbol = "X"
+victory = False
+while not victory:
+    draw_grid(values)
+    while True:
+        try:
+            answer = input(f"Vous etes le joueur selectionnez {player_symbol} Ou voulez vous jouer Atention c'est a vos rique et perille").split(" ")
+            values[int(answer[0])][int(answer[1])] = player_symbol
+            break
+        except:
+            print("Mauvaise position")
+    if player_symbol == "X":
+       player_symbol = "O"
+    else:
+       player_symbol = "X"
