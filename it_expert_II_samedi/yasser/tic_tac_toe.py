9a4dea7446392cdccb8d57373838c41d987b9cb1
student <student@gmail.com>
2025-09-27 17:52:41 +0200

    lesson 5

---
+++ it_expert_II_samedi/yasser/tic_tac_toe.py
@@ -0,0 +1,33 @@
+
+def draw_grid(values):
+    for j in range(3):
+        for i in range(3):
+            if i == 1:
+                print(f"   {values[j][0]}   |   {values[j][1]}   |   {values[j][2]}   ")
+            else:
+                print("       |       |       ")
+        if j < 2:
+            print("-------+-------+-------")
+values = [[" ", " ", " "],
+          [" ", " ", " "],
+          [" ", " ", " "]]
+player_symbol = "X"
+while True:
+    draw_grid(values)
+    while True:
+        answer = input(f"Vous êtes le joueur {player_symbol}, doù voulez-vous jouer ? ")
+        if answer == "quit":
+            print("Au revoir!")
+            break
+        else:
+            try:
+                numbers = answer.split(" ")
+                values[int(numbers[0])][int(numbers[1])] = player_symbol #ici (je pense)
+                break
+            except:
+                print("Mauvaise position, réessaie !")
+
+    if player_symbol == "X":
+        player_symbol = "O"
+    else:
+        player_symbol = "X"
