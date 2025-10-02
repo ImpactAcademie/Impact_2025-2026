535012fa27e1cf0c718e6e44012d8b6536c93a3f
Yperite <quentinlaperche0@gmail.com>
2025-10-02 19:00:10 +0200

    pong

---
+++ it_expert_II_jeudi/prof/test_classes.py
@@ -0,0 +1,17 @@
+class Maison:
+    def __init__(self, length):
+        self.nbWindows = length//3
+        self.length, self.height = length, 5
+
+class Cabane(Maison):
+    def __init__(self):
+        super().__init__(5)
+        self.nbWindows = 0
+
+m1 = Maison(10)
+m2 = Maison(25)
+m3 = Cabane()
+
+print(m1.nbWindows)
+print(m2.nbWindows)
+print(m3.nbWindows)
