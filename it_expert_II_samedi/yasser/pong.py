5fb7894feeef0815b296cb6d5ef82eae3c879c24
student <student@gmail.com>
2025-10-11 18:00:19 +0200

    calc

--- it_expert_II_samedi/yasser/pong.py
+++ it_expert_II_samedi/yasser/pong.py
@@ -11,9 +11,31 @@ BLACK = (0, 0, 0)
 
 # Une classe GameObject crée avec (x, y, width, height, color) avec méthode draw(screen)
 """Classe de base pour tout objet ayant un rectangle et une couleur"""
+class Game_object:
+    def __init__(self,x,y,width,height,color):
+        self.color=color
+        self.rect=pygame.Rect(x,y,width,height)
+    def draw(self,screen):
+        pygame.draw.rect(screen,self.color,self.rect)
+
+        
+        
 
 # Une classe Paddle qui hérite de GameObject crée avec (x, y) de taille (15, 100), couleur (WHITE) et vitesse (5) avec méthode move(up_key, down_key, keys)
 """Raquette qui se déplace verticalement"""
+class Paddle(Game_object):
+    def __init__(self, x, y, ):
+        super().__init__(x, y, 15, 100, WHITE)
+        self.speed=5
+
+    def move(self,up_key, down_key, keys):
+        if keys[up_key] and self .rect.top>0:
+            self.rect.y-=self.speed
+
+        if keys[down_key] and self .rect.bottom<SCREEN_HEIGHT:
+            self.rect.y+=self.speed
+
+
 
 # Une classe Ball qui hérite de GameObject crée avec (x, y) de taille (15, 15) et couleur (WHITE) de vitesse (4, 4) avec une méthode update(paddles, scores): Bouger, Rebondir sur les platformes et le murs, gérer les sorties
 """Balle qui rebondit sur les murs et les raquettes"""
@@ -29,6 +51,10 @@ def main():
 
     # TODO crée left_paddle, right_paddle and ball
 
+    left_paddle=Paddle(30,SCREEN_HEIGHT//2-50)
+    right_paddle=Paddle(SCREEN_WIDTH-45,SCREEN_HEIGHT//2-50)
+    ball=''
+
     scores = [0, 0]  # [gauche, droite]
 
     running = True
@@ -40,6 +66,8 @@ def main():
 
         keys = pygame.key.get_pressed()
         # TODO move the paddles and update the ball
+        left_paddle.move(pygame.K_z,pygame.K_s,keys)
+        right_paddle.move(pygame.K_UP,pygame.K_DOWN,keys)
 
         screen.fill(BLACK)
 
@@ -48,6 +76,8 @@ def main():
 
         # dessiner objets
         # TODO
+        left_paddle.draw(screen)
+        right_paddle.draw(screen)
 
         # afficher score
         score_left_text = font.render(str(scores[0]), True, WHITE)
