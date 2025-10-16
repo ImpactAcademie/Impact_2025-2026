03ebad6ea10c381c733aa1a2d63fa06456dffd7d
student <student@gmail.com>
2025-10-16 19:06:56 +0200

    Calculatrice

--- it_expert_II_jeudi/romain/calculatrice.py
+++ it_expert_II_jeudi/romain/calculatrice.py
@@ -3,6 +3,8 @@ from kivy.uix.label import Label
 from kivy.uix.image import Image, AsyncImage
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+from kivy.uix.gridlayout import GridLayout
 
 class BasicApp(App):
     def build(self):
@@ -21,7 +23,32 @@ class HBoxLayout(App):
         for i in range(5):
             layout.add_widget(Button(text="Bouton", background_color=[1, 0, 0, 1]))
         return layout
+    
+class Calculator(App):
+    def build(self):
+        textinput = TextInput()
+
+        grid = GridLayout(cols=4)
+        values = ["7", "8", "9", "+", "4", "5", "6", "*", "1", "2", "3", "-", "C", "0", "=", "/"]
+        def add_value(instance):
+            if instance.text == "C":
+                textinput.text = ""
+            elif instance.text == "=":
+                textinput.text = str(eval(textinput.text))
+            else:
+                textinput.text += instance.text
+        for v in values:
+            button = Button(text=v, background_color=(1, 0.07, 0.57, 1))
+            button.bind(on_press=add_value)
+            grid.add_widget(button)
+      
+        layout = BoxLayout(orientation="vertical")
+        layout.add_widget(textinput)
+        layout.add_widget(grid)
+
+        return layout 
 
 
-app = ImageApp()
+app = Calculator()
 app.run()
+
