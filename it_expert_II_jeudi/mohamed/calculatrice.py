a0ed05cf2fa0c32639f645a3b2ceb1602ac800fc
student <student@gmail.com>
2025-10-16 19:08:36 +0200

    Calculatrice

--- it_expert_II_jeudi/mohamed/calculatrice.py
+++ it_expert_II_jeudi/mohamed/calculatrice.py
@@ -3,6 +3,8 @@ from kivy.uix.label import Label
 from kivy.uix.image import Image
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+from kivy.uix.gridlayout import GridLayout
 
 class BasicApp(App):
     def build(self):
@@ -22,7 +24,30 @@ class HBoxLayout(App):
             layout.add_widget(Button(text="Bouton", background_color=[1, 0, 0, 1]))
         return layout
 
+class Calculator(App):
+    def build(self):
+        textinput = TextInput()
 
-    
-app = HBoxLayout()
+        grid = GridLayout(cols=4)
+        values = ["7", "8", "9", "+", "4", "5", "6", "*", "1", "2", "3", "-", "C", "0", "=", "/"]
+        def add_value(instance):
+            if instance.text == "C":
+                textinput.text =""
+            elif instance.text == "=":
+                textinput.text = str(eval(textinput.text))
+            else:
+                textinput.text += instance.text
+        for v in values:
+               button  = Button(text=v, font_size=50, background_color=(1, 0, 0, 1))
+               button.bind(on_press=add_value)
+               grid.add_widget(button)
+
+       
+        layout = BoxLayout(orientation="vertical")
+        layout.add_widget(textinput)
+        layout.add_widget(grid)
+
+        return layout
+
+app = Calculator()
 app.run()
