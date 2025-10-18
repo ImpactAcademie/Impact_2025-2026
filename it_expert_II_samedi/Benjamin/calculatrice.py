33a251505b26ae953aa73888b88dc1270ef1ef3e
student <student@gmail.com>
2025-10-18 17:59:53 +0200

    signup

--- it_expert_II_samedi/Benjamin/calculatrice.py
+++ it_expert_II_samedi/Benjamin/calculatrice.py
@@ -1,15 +1,36 @@
 from kivy.app import App
-from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
-from kivy.lang.builder import Builder
-class Calc(App):
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.textinput import TextInput
+
+entry = ""
+
+class Calculator(App):
     def build(self):
-        grid = GridLayout(cols=4)
-        texts = ["7","8","9","+","4","5","6","-","1","2","3","*","C","0","=","/"]
+        label = TextInput(text="", size_hint=(1, 0.5))
+        grid = GridLayout(cols=4, padding=10, spacing=10)
+        buttonList = ("7","8","9","+","4","5","6","-","1","2","3","*","C","0","=","/")
         for i in range(16):
-            grid.add_widget(Button(text="Cliquez moi", font_size= 20))
-        return grid
-        # return Builder.load_file("calc.kv")
-    
-calc = Calc()
-calc.run()
+            def callback(instance):    
+                if instance.text == "C":
+                    label.text = ""
+                elif instance.text == "=" and label.text[-1] in ["+","-","*","/"]:
+                    return
+                elif instance.text == "=":
+                    label.text = str(eval(label.text))
+                elif instance.text in ["+","-","*","/"] and label.text[-1] in ["+","-","*","/"]:
+                    return
+                else:
+                    label.text += instance.text
+            button = Button(text=buttonList[i], font_size=30)
+            button.bind(on_press=callback)
+            grid.add_widget(button)
+        box = BoxLayout(orientation="vertical")
+        box.add_widget(label)
+        box.add_widget(grid)
+        return box
+if __name__ == "__main__":
+    calc = Calculator()
+    calc.run()
