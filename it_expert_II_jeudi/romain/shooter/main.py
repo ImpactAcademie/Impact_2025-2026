35a6eb6a4e23f77edcea099149f2bc68165429fe
student <student@gmail.com>
2025-11-13 19:03:07 +0100

    shooter2

--- it_expert_II_jeudi/romain/shooter/main.py
+++ it_expert_II_jeudi/romain/shooter/main.py
@@ -3,10 +3,23 @@ import math
 from sys import exit
 
 
-class Player:
+class Entity:
+    def __init__(self, pos, angle, speed):
+        self.pos =list(pos)
+        self.angle = angle
+        self.speed = speed
+
+    def move(self, dt, angle=None):
+        self.pos[0] += math.cos(self.angle if angle == None else angle)*self.speed*dt
+        self.pos[1] -= math.sin(self.angle if angle == None else angle)*self.speed*dt
+
+    def set_angle(self, angle):
+        self.angle = angle
+
+
+class Player(Entity):
     def __init__(self):
-        self.pos = [675,375]
-        self.angle = 0
+        super(). __init__([700,400], 0, 250)     
         self.health = 100
         self.maxHealth = 100
         self.image = pygame.transform.rotozoom(pygame.image.load("player_gun.png"), 0, 0.5)
@@ -15,54 +28,73 @@ class Player:
     def look_towards(self, point):
          x = point[0]-self.pos[0]
          y = point[1]-self.pos[1]
-         self.angle = math.atan2(-y, x)
+         self.set_angle(math.atan2(-y, x))
          self.currentImage = pygame.transform.rotate(self.image, self.angle/math.pi*180)
 
     def shoot(self, projectiles):
-        projectiles.append(Bullet(self.pos, self.angle))
-
-    def move(self, x, y):
-        self.pos[0] += x
-        self.pos[1] += y 
+        projectiles.append(Bullet(self.pos, self.angle)) 
 
     def draw(self, screen):
         screen.blit(self.currentImage, self.currentImage.get_rect(center=self.pos))
 
 
-class Bullet:
+class Bullet(Entity):
     def __init__(self, pos, angle):
-        self.pos = pos
-        self.angle = angle
+        super().__init__(pos, angle, 800)
+    
+    def draw(self, screen):
+        y = math.sin(-self.angle)*8+self.pos[1]
+        x = math.cos(self.angle)*8+self.pos[0]
+        pygame.draw.line(screen, (255, 0, 0), self.pos, (x, y), 3)
+      
 
 
 
 pygame.init()
-screen = pygame.display.set_mode((1350,750))
+screen = pygame.display.set_mode((1400,800))
 clock = pygame.time.Clock()
 player = Player()
+projectiles = []
 
 while True:
+    dt = clock.tick(120)/1000
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
         elif event.type == pygame.MOUSEMOTION:
             player.look_towards(pygame.mouse.get_pos())
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                player.shoot(projectiles)
 
     pressed = pygame.key.get_pressed()
     if pressed[pygame.K_z]:
-        player.move(0, -5)
+        player.move(dt)
+        player.look_towards(pygame.mouse.get_pos())
     if pressed[pygame.K_s]:
-        player.move(0, 5)
+        player.move(dt, angle=player.angle+math.pi)
+        player.look_towards(pygame.mouse.get_pos())
     if pressed[pygame.K_q]:
-        player.move(-5, 0)
+        player.move(dt, angle=player.angle+math.pi/2)
+        player.look_towards(pygame.mouse.get_pos())
     if pressed[pygame.K_d]:
-        player.move(5, 0)
+        player.move(dt, angle=player.angle-math.pi/2)
+        player.look_towards(pygame.mouse.get_pos())
+    for projectile in projectiles:
+        projectile.move(dt)
 
+    for projectile in projectiles[:]:
+        if projectile.pos[0] < 0 or projectile.pos[0] > screen.get_width():
+            projectiles.remove(projectile)
+        elif projectile.pos[1] < 0 or projectile.pos[1] > screen.get_height():
+                projectiles.remove(projectile)
 
-        
+    
 
     screen.fill((100, 50, 150))
+    for projectile in projectiles:
+        projectile.draw(screen)
     player.draw(screen)
     pygame.display.update()
-    clock.tick(120)
