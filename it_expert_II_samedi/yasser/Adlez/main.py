4ffbc2d0b2dc74d06efc2c3a21c8a980eebef704
student <student@gmail.com>
2025-11-22 18:03:34 +0100

    adlez

---
+++ it_expert_II_samedi/yasser/Adlez/main.py
@@ -0,0 +1,26 @@
+import pygame
+from sys import exit
+import math
+
+from animation import Animation
+
+
+pygame.init()
+screen = pygame.display.set_mode((1200,700))
+clock = pygame.time.Clock()
+
+animation1 = Animation("assets/images/walk/F", 6, 5)
+animation1.start()
+while True:
+    dt = clock.tick(60)/1000
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    animation1.update()
+
+    screen.fill((10,10,30))
+    animation1.draw(screen, (600, 350))
+    pygame.display.update()
+
